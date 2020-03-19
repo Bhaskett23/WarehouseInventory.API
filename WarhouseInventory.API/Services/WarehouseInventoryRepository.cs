4fc8062ee9163b0a8c52744b1bebeaaf62147f08
Brandon Haskett <brhaskett@yahoo.com>
2020-03-18 21:06:08 -0400

    setting up back end of api

---
+++ WarhouseInventory.API/Services/WarehouseInventoryRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WarehouseInventory.API.Context;
+using WarehouseInventory.API.Models;
+
+namespace WarehouseInventory.API.Services
+{
+    public class WarehouseInventoryRepository : IWarehouseInventoryRepository
+    {
+        private readonly WarehouseInventoryContext _context;
+
+        public WarehouseInventoryRepository()
+        {
+            _context = new WarehouseInventoryContext();
+        }
+
+        public Item GetItem(int itemId)
+        {
+            return _context.Items.FirstOrDefault(x => x.Id == itemId);
+        }
+
+        public IEnumerable<Item> GetItems()
+        {
+            return _context.Items;
+        }
+    }
+}
